fcaf44415bf411a14902ee25c9954d073ade9280
Ubuntu <ubuntu@ip-172-31-41-46.ec2.internal>
2022-04-01 18:46:32 +0000

    Added hello World

---
+++ helloWorld.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2022 Ethan Prieur All rights reserved
+//
+// Created by: Ethan Prieur
+// Created on: April 2022
+// This is the "Hello, World!" program, with proper style
+
+#include <iostream>
+
+int main() {
+  std::cout << "Hello, World!";
+}
